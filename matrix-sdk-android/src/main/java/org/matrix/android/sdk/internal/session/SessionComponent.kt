71528aacc8091e719f17c01c7d9700dca7917bf3
Valere <valeref@matrix.org>
2021-01-15 09:57:29 +0100

    MSC 2946 WIP

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/SessionComponent.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/SessionComponent.kt
@@ -51,6 +51,7 @@ import org.matrix.android.sdk.internal.session.room.send.RedactEventWorker
 import org.matrix.android.sdk.internal.session.room.send.SendEventWorker
 import org.matrix.android.sdk.internal.session.search.SearchModule
 import org.matrix.android.sdk.internal.session.signout.SignOutModule
+import org.matrix.android.sdk.internal.session.space.SpaceModule
 import org.matrix.android.sdk.internal.session.sync.SyncModule
 import org.matrix.android.sdk.internal.session.sync.SyncTask
 import org.matrix.android.sdk.internal.session.sync.SyncTokenStore
@@ -89,7 +90,8 @@ import org.matrix.android.sdk.internal.util.MatrixCoroutineDispatchers
             AccountModule::class,
             CallModule::class,
             SearchModule::class,
-            ThirdPartyModule::class
+            ThirdPartyModule::class,
+            SpaceModule::class
         ]
 )
 @SessionScope
